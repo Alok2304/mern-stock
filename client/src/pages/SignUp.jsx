fb667c62d71c0720b6f37df305dfcb4d8b607ce0
Alok Sandip Kale <alokkale2304@gmail.com>
2024-05-13 12:06:38 +0530

    Added functionality to the SignUp page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,5 +1,7 @@
+import Alert from "@mui/material/Alert";
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
+import CircularProgress from "@mui/material/CircularProgress";
 import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
 import Link from "@mui/material/Link";
@@ -9,6 +11,8 @@ import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 // function Copyright(props) {
 // 	return (
@@ -31,6 +35,48 @@ import { createTheme, ThemeProvider } from "@mui/material/styles";
 const defaultTheme = createTheme();
 
 export default function SignUp() {
+	const [formData, setFormData] = useState({});
+	const [errorMessage, setErrorMessage] = useState(null);
+	const [loading, setLoading] = useState(false);
+	const navigate = useNavigate();
+
+	const handleChange = (e) => {
+		setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+	};
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		if (
+			!formData.firstName ||
+			!formData.lastName ||
+			!formData.email ||
+			!formData.password
+		) {
+			return setErrorMessage("Please fill all the required fields");
+		}
+		try {
+			setLoading(true);
+			setErrorMessage(null);
+			const res = await fetch("/api/auth/signup", {
+				method: "POST",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify(formData),
+			});
+			const data = await res.json();
+			if (data.success === false) {
+				return setErrorMessage(data.message);
+			}
+			setLoading(false);
+			if (res.ok) {
+				navigate("/sign-in");
+			}
+		} catch (error) {
+			setErrorMessage(error.message);
+			setLoading(false);
+		} finally {
+			setLoading(false);
+		}
+	};
 
 	return (
 		<ThemeProvider theme={defaultTheme}>
@@ -53,7 +99,8 @@ export default function SignUp() {
 					<Box
 						component="form"
 						noValidate
-						sx={{ mt: 3 }}
+						sx={{ mt: 3, mb: 2 }}
+						onSubmit={handleSubmit}
 					>
 						<Grid container spacing={2}>
 							<Grid item xs={12} sm={6}>
@@ -65,6 +112,7 @@ export default function SignUp() {
 									id="firstName"
 									label="First Name"
 									autoFocus
+									onChange={handleChange}
 								/>
 							</Grid>
 							<Grid item xs={12} sm={6}>
@@ -75,6 +123,7 @@ export default function SignUp() {
 									label="Last Name"
 									name="lastName"
 									autoComplete="family-name"
+									onChange={handleChange}
 								/>
 							</Grid>
 							<Grid item xs={12}>
@@ -85,6 +134,7 @@ export default function SignUp() {
 									label="Email Address"
 									name="email"
 									autoComplete="email"
+									onChange={handleChange}
 								/>
 							</Grid>
 							<Grid item xs={12}>
@@ -96,6 +146,7 @@ export default function SignUp() {
 									type="password"
 									id="password"
 									autoComplete="new-password"
+									onChange={handleChange}
 								/>
 							</Grid>
 						</Grid>
@@ -104,17 +155,19 @@ export default function SignUp() {
 							fullWidth
 							variant="contained"
 							sx={{ mt: 3, mb: 2 }}
+							disabled={loading}
 						>
-							Sign Up
+							{loading ? <CircularProgress color="inherit" /> : "Sign Up"}
 						</Button>
 						<Grid container justifyContent="flex-end">
-							<Grid item>
+							<Grid item sx={{ marginBottom: "1.5 rem" }}>
 								<Link href="/sign-in" variant="body2">
 									Already have an account? Sign in
 								</Link>
 							</Grid>
 						</Grid>
 					</Box>
+					{errorMessage && <Alert severity="error">{errorMessage}</Alert>}
 				</Box>
 			</Container>
 		</ThemeProvider>
