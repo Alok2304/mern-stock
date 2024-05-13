414b32f76f600c7fa6110dce59685163b9e09f33
Alok Sandip Kale <alokkale2304@gmail.com>
2024-05-13 13:56:18 +0530

    Completed SignIn page UI and functionality

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,7 +1,149 @@
-import React from 'react'
+import Alert from "@mui/material/Alert";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CircularProgress from "@mui/material/CircularProgress";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+// import FormControlLabel from "@mui/material/FormControlLabel";
+// import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+// function Copyright(props) {
+//   return (
+//     <Typography variant="body2" color="text.secondary" align="center" {...props}>
+//       {'Copyright © '}
+//       <Link color="inherit" href="https://mui.com/">
+//         Your Website
+//       </Link>{' '}
+//       {new Date().getFullYear()}
+//       {'.'}
+//     </Typography>
+//   );
+// }
+
+// TODO remove, this demo shouldn't need to reset the theme.
+
+const defaultTheme = createTheme();
 
 export default function SignIn() {
-  return (
-    <div>SignIn</div>
-  )
+	const [formData, setFormData] = useState({});
+	const [errorMessage, setErrorMessage] = useState(null);
+	const [loading, setLoading] = useState(false);
+	const navigate = useNavigate();
+
+	const handleChange = (e) => {
+		setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+	};
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		if (!formData.email || !formData.password) {
+			return setErrorMessage("Please fill all the required fields");
+		}
+		try {
+			setLoading(true);
+			setErrorMessage(null);
+			const res = await fetch("/api/auth/signin", {
+				method: "POST",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify(formData),
+			});
+			const data = await res.json();
+			if (data.success === false) {
+				return setErrorMessage(data.message);
+			}
+			setLoading(false);
+			if (res.ok) {
+				navigate("/");
+			}
+		} catch (error) {
+			setErrorMessage(error.message);
+			setLoading(false);
+		} finally {
+      setLoading(false);
+    }
+
+	};
+
+	return (
+		<ThemeProvider theme={defaultTheme}>
+			<Container component="main" maxWidth="xs">
+				<CssBaseline />
+				<Box
+					sx={{
+						marginTop: 8,
+						display: "flex",
+						flexDirection: "column",
+						alignItems: "center",
+					}}
+				>
+					<Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+						<LockOutlinedIcon />
+					</Avatar>
+					<Typography component="h1" variant="h5">
+						Sign in
+					</Typography>
+					<Box
+						component="form"
+						onSubmit={handleSubmit}
+						noValidate
+						sx={{ mt: 1 , mb: 2}}
+					>
+						<TextField
+							margin="normal"
+							required
+							fullWidth
+							id="email"
+							label="Email Address"
+							name="email"
+							autoComplete="email"
+							autoFocus
+							onChange={handleChange}
+						/>
+						<TextField
+							margin="normal"
+							required
+							fullWidth
+							name="password"
+							label="Password"
+							type="password"
+							id="password"
+							autoComplete="current-password"
+							onChange={handleChange}
+						/>
+						{/* <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            /> */}
+						<Button
+							type="submit"
+							fullWidth
+							variant="contained"
+							sx={{ mt: 3, mb: 2 }}
+              disabled={loading}
+						>
+							{loading ? <CircularProgress color="inherit" /> : "Sign Up"}
+						</Button>
+						<Grid container>
+							<Grid item>
+								<Link href="/sign-up" variant="body2">
+									{"Don't have an account? Sign Up"}
+								</Link>
+							</Grid>
+						</Grid>
+					</Box>
+				</Box>
+        {errorMessage && <Alert severity="error">{errorMessage}</Alert>}
+				{/* <Copyright sx={{ mt: 8, mb: 4 }} /> */}
+			</Container>
+		</ThemeProvider>
+	);
 }
