dc8dbe9aa270850d4335d6216ecc8e4b4ec0eb5e
Alok Sandip Kale <alokkale2304@gmail.com>
2024-05-14 18:55:58 +0530

    Corrected some minor bugs

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,6 +1,6 @@
 import { configureStore , combineReducers } from '@reduxjs/toolkit';
 import userReducer from "./user/userSlice.js";
-import {persistReducer, persistStore} from "redux-perist";
+import {persistReducer, persistStore} from "redux-persist";
 import storage from "redux-persist/lib/storage";
 
 const rootReducer = combineReducers({
