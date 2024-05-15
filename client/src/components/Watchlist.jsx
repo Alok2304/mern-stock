633fa2a45ef2d69e56c1e8c0ecca67e643388cc7
Alok Sandip Kale <alokkale2304@gmail.com>
2024-05-16 00:40:06 +0530

    Updated Components part 2

--- client/src/components/Watchlist.jsx
+++ client/src/components/Watchlist.jsx
@@ -7,6 +7,14 @@ export default function Watchlist() {
 				<h1 className='title'>
 					Your WatchList
 				</h1>
+				<div className="lists">
+					<h1 className='stock-symbol'>IBM</h1>
+					<p className='stock-symbol'>Open: </p>
+					<p className='stock-symbol'>High: </p>
+					<h1 className='stock-symbol'>Low: </h1>
+					<h1 className='stock-symbol'>Close: </h1>
+					<h1 className='stock-symbol'>Volume: </h1>
+				</div>
 			</div>
 		</div>
 	);
